e46e41d1871b1ef179b212bf512977fc7dce419f
Danhalf <danhel@rocketmail.com>
2021-06-05 20:08:30 +0300

    v0.3

--- index.js
+++ index.js
@@ -1,46 +1,77 @@
+
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const schedule = require("node-schedule");
+const rule = new schedule.RecurrenceRule();
+let count = 0;
+//rule.minute = 40;
+rule.second = 10;
+schedule.scheduleJob(rule, function () {
+  console.log(`Обновлений: ${count}`);
+  count++;
+  const url = 'https://www.ruptela.ua/novini/';
 
-const url = 'https://www.ruptela.ua/novini/';
-// const url = 'https://news.ycombinator.com/';
-// https://www.ruptela.ua/novini/
 
-// const http = require('http');
+  let linkData = {};
+  let data = {};
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
+  axios
+    .get(url)
+    .then((r) => getData(r.data))
+    .catch((error) => console.log(error));
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+  let getData = (html) => {
 
-axios
-  .get(url)
-  .then((r) => getData(r.data))
-  .catch((error) => console.log(error));
+    const $ = cheerio.load(html);
+    $('.news-item-wrap').each((i, elem) => {
+      data[i] = {
+        title: $(elem).find('.name').text().replace(/\t/g, '').replace(/\n/g, ''),
+        new: $(elem).find('.excerpt').text(),
+        img: $(elem).find('.wp-post-image').attr('data-src'),
+        link: $(elem).find('a').attr('href'),
+        // link: $(elem).find('a.storylink').attr('href'),
+      };
+    })
 
-let getData = (html) => {
-  data = {};
-  const $ = cheerio.load(html);
-  $('.news-item-wrap').each((i, elem) => {
-    data[i] = {
-      title: $(elem).find('.name').text().replace(/\t/g, '').replace(/\n/g, ''),
-      new: $(elem).find('.excerpt').text(),
-      img: $(elem).find('.wp-post-image').attr('data-src'),
-      link: $(elem).find('a').attr('href'),
-      // link: $(elem).find('a.storylink').attr('href'),
-    };
-  });
 
-  fs.writeFile('news.js', `export default ${JSON.stringify(data)}`, (err) => {
-    if (err) throw err;
-    console.log('Data has been added!');
-  });
-};
+
+    fs.writeFile('news.js', `export default ${JSON.stringify(data)}`, (err) => {
+      if (err) throw err;
+      console.log('Data has been added!');
+    })
+
+  }
+
+
+  setTimeout(() => {
+    for (let index in data) {
+      axios.get(data[index].link)
+        .then(response => getInner(response.data))
+        .catch((error) => console.log(error));
+
+      let getInner = (html) => {
+        const $ = cheerio.load(html);
+        $('.main-content').each((i, elem) => {
+          linkData[index] = {
+            fullNewsTitle: $(elem).find('.page-title').text(),
+            fullNew: $(elem).find('.wpb_content_element p').text(),
+
+          };
+        })
+      }
+    }
+
+  }, 2000)
+
+  setTimeout(() => {
+    fs.writeFile('fullNews.js', `export default ${JSON.stringify(linkData)}`, (err) => {
+      if (err) throw err;
+      console.log('Fullnews data has been added!');
+    })
+  }, 5000)
+
+
+});
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import news from './news.js';
+import fullNews from './fullNews.js';
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 8; i++) {
   let div = document.createElement('div');
   document.body.append(div);
-  div.classList.add(`new-${i}`);
-  div.innerHTML = `<strong>${news[i].title}</strong><div>${news[i].new}</div><img src='${news[i].img}'><a href='${news[i].link}'>Дізнатись більше</a>`;
+  div.classList.add(`new-item-${i}`);
+  div.innerHTML = `<h2 class='news-title'>${fullNews[i]?.fullNewsTitle}</h2><div class='news-content'><img class='news-img' align='left' src='${news[i].img}'>${fullNews[i]?.fullNew}</div>`;
 }
